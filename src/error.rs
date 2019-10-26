23139841ce39c0f517fbb72fbb297aeecdd02ed8
Povilas Balciunas <balciunas90@gmail.com>
2019-10-26 16:19:12 +1300

    Split lib into modules

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use err_derive::Error;
+use std::str::Utf8Error;
+
+#[derive(Debug, Error)]
+/// `DiscoveryMsg` deserialization error.
+pub enum DeserializeError {
+    /// Not enough bytes to parse some specific field.
+    #[error(display = "Not enough bytes to parse field '{}'. Expected {}, \
+      remaining {} at position {}.", _0, _1, _2, _3)]
+    NotEnoughBytes(String, usize, usize, usize),
+    #[error(display = "Failed to parse UTF-8 field '{}'. Error: {}", _0, _1)]
+    InvalidUtf8(String, Utf8Error),
+    #[error(display = "Unknown transport protocol: {}", _0)]
+    UnknownProtocol(u8),
+}
+
+#[derive(Debug, Error)]
+/// Peer discovery errors.
+pub enum Error {
+    /// Too long data item key.
+    #[error(display = "Too long item key. Max length is 255 bytes.")]
+    TooLongKey,
+    /// Data item limit reached - 255.
+    #[error(display = "Data item limit reached - 255.")]
+    TooManyDataItems,
+    /// Deserialization error.
+    #[error(display = "Failed to deserialize DiscoveryMsg from bytes: {}", _0)]
+    Deserialize(DeserializeError),
+}
+
+impl From<DeserializeError> for Error {
+    fn from(e: DeserializeError) -> Self {
+        Error::Deserialize(e)
+    }
+}
