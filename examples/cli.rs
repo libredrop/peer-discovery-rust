09665979ec2315b38b008f99e596ede89f462a0a
Povilas Balciunas <balciunas90@gmail.com>
2019-11-17 16:21:34 +1300

    Use task executor from async-std
    Makes things simpler. Also, async-std sounds promising in general.

--- examples/cli.rs
+++ examples/cli.rs
@@ -2,6 +2,7 @@
 //! remote peers.
 //! You can use this example to test peer discovery on your LAN.
 
+use async_std::task;
 use futures::{future, StreamExt};
 use log;
 use peer_discovery::{discover_peers, DiscoveryMsg, TransportProtocol};
@@ -10,11 +11,10 @@ use unwrap::unwrap;
 
 fn main() {
     unwrap!(simple_logger::init_with_level(log::Level::Info));
-    let mut executor = futures::executor::LocalPool::new();
 
     let msg = DiscoveryMsg::new("discovery-cli".into(), TransportProtocol::Tcp, 5000);
-    let rx_msgs = unwrap!(discover_peers(&mut executor, msg));
-    executor.run_until(rx_msgs.for_each(|msg| {
+    let rx_msgs = unwrap!(discover_peers(msg));
+    task::block_on(rx_msgs.for_each(|msg| {
         println!("Received: {:?}", msg);
         future::ready(())
     }));
